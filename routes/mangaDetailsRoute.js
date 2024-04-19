791a09a59af9a603cb3f6991bdb692059eba3ea8
unknown <shakthivelmanjeedan11@gmail.com>
2024-04-19 20:18:29 +0530

    Added CRUD logics

--- routes/mangaDetailsRoute.js
+++ routes/mangaDetailsRoute.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const MangaDetails = require('./models/mangaDetails');
+const { mangaSchema } = require("../models/mangaDetails");
+const MangaDetails = require("../models/mangaDetails");
 
-// Route to add manga details to the database
-router.post('/add', async (req, res) => {
+
+router.post('/', async (req, res) => {
     try {
-        const { title, author, demographic, genre } = req.body;
+        const { title, author, demographic, genre, synopsis } = req.body;
         
-        // Create a new manga details document
+        
         const newManga = new MangaDetails({
             title,
             author,
             demographic,
-            genre
+            genre,
+            synopsis
         });
 
-        // Save the new manga details document to the database
         const savedManga = await newManga.save();
 
         res.status(201).json(savedManga);
     } catch (err) {
         console.error(err);
+        if (err.name === 'ValidationError') {
+            return res.status(400).json({ message: err.message });
+        }
         res.status(500).json({ message: "Internal server error" });
     }
 });
